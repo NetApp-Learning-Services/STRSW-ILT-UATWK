b16cb1642d8004e53071f065bdf6def873325cdc
Curtis Burchett <curtis@burchett.me>
2024-10-18 22:00:56 +0000

    Working on Exercise 6

--- Exercise 6/exercise6Task4-4.sh
+++ Exercise 6/exercise6Task4-4.sh
@@ -1,6 +1,6 @@
 BUCKETKEY=change_me
 BUCKETSECRET=change_me
 
-kubectl create secret generic -n trident-protect s3-src-creds \
+kubectl create secret generic -n trident-protect s3-dst-creds \
   --from-literal=accessKeyID=$BUCKETKEY \
   --from-literal=secretAccessKey=$BUCKETSECRET
