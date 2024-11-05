c26824e7e111f01bdd2f1beadbc6bd5a0df94f02
Curtis Burchett <curtis@burchett.me>
2024-11-05 23:10:52 +0000

    Working on demo

--- Extras/tpDemo/dest/exercise5Task2-1.sh
+++ Extras/tpDemo/dest/exercise5Task2-1.sh
@@ -1,6 +1,6 @@
-BUCKETKEY=change_me
-BUCKETSECRET=change_me
+BUCKETKEY2=0CQQK317HZKNWLYHC42L
+BUCKETSECRET2=hZdAzz_ng5xNtSAe64_Ph_gcDM_Cg33445CO0pdI
 
-kubectl create secret generic -n trident-protect s3-src-creds \
-  --from-literal=accessKeyID=$BUCKETKEY \
-  --from-literal=secretAccessKey=$BUCKETSECRET
+kubectl create secret generic -n trident-protect s3-dest-creds \
+  --from-literal=accessKeyID=$BUCKETKEY2 \
+  --from-literal=secretAccessKey=$BUCKETSECRET2
--- Extras/tpDemo/src/exercise5Task2-1.sh
+++ Extras/tpDemo/src/exercise5Task2-1.sh
@@ -1,5 +1,5 @@
-BUCKETKEY=change_me
-BUCKETSECRET=change_me
+BUCKETKEY=Q5BD141RZBUH70T2V6PS
+BUCKETSECRET=7ax3tUyGlaCFA2Dn9YpQp3c7A2RWZJ_c1TcsCTxz
 
 kubectl create secret generic -n trident-protect s3-src-creds \
   --from-literal=accessKeyID=$BUCKETKEY \
