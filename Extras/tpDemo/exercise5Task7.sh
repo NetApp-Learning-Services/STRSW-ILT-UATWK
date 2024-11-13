3d69f6e2af333a9dfa778733a094f3a618aaa330
Curtis Burchett <curtis@burchett.me>
2024-11-13 03:47:15 +0000

    Updated to the new tridentctl-protect

--- Extras/tpDemo/exercise5Task7.sh
+++ Extras/tpDemo/exercise5Task7.sh
@@ -1,15 +1,10 @@
 echo "############################################"
-echo "###         Protectctl install           ###"
+echo "###         tridentctl install           ###"
 echo "############################################"
 
-ACCOUNTID=d13e74bf-3ced-488d-b31a-8ed307395e93
-TOKEN=hwTgNdnLXQEa8mD5h_YxOSuhBNakxQfA-npBgHU6Dsg=
-PROTECTVERSION=24.10.0-preview.1
-
-podman login -u $ACCOUNTID -p $TOKEN cr.astra.netapp.io
-podman pull cr.astra.netapp.io/trident-protectctl:$PROTECTVERSION
-podman run --rm -v $(pwd):/pctl-tmp cr.astra.netapp.io/trident-protectctl:$PROTECTVERSION cp protectctl /pctl-tmp
-mv protectctl /usr/local/bin
+curl -L -o tridentctl-protect https://github.com/NetApp/tridentctl-protect/releases/download/24.10.0/tridentctl-protect-linux-amd64
+chmod +x tridentctl-protect
+sudo mv ./tridentctl-protect /usr/local/bin/
 
 mkdir -p ~/.trident-protect
 echo 'namespace: trident-protect' > ~/.trident-protect/protectctl.yaml
