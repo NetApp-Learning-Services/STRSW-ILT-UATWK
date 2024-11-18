3ea2dc80c1f6f5722080f4a042801a7497733327
Curtis Burchett <curtis@burchett.me>
2024-11-18 18:49:26 +0000

    Added prerequistes to the Exercise 1 and 2

---
+++ Exercise 1/Prerequisites/exercise0Task4.sh
@@ -0,0 +1,18 @@
+# This script has been written for this exercise environment 
+# and is not intented to be used in a production enviroment
+# Execute by: ./exercise0Task4.sh
+
+#!/bin/bash
+DIR='/home/user/.kube' 
+sudo apt install sshpass
+sshpass -p Netapp1! scp -o "StrictHostKeyChecking=no" root@kubmas1-1:/root/.kube/config config1 
+sshpass -p Netapp1! scp -o "StrictHostKeyChecking=no" root@kubmas2-1:/root/.kube/config config2
+sudo sed -i 's/\<kubernetes\>/source/g' config1 
+sudo sed -i 's/\<kubernetes\>/destination/g' config2
+konfig=$(KUBECONFIG=config1:config2 kubectl config view --flatten)
+if [ ! -d "$DIR" ]; then
+    mkdir -p $DIR;
+fi
+echo "$konfig" > $DIR/config
+#echo "$konfig" > config
+$KUBECONFIG=$DIR/config
