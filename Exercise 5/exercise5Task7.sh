6be01881866f18f54367b2136cd57794659b4ade
Curtis Burchett <curtis@burchett.me>
2024-11-21 22:37:02 +0000

    Updated tridentctl-protect install

--- Exercise 5/exercise5Task7.sh
+++ Exercise 5/exercise5Task7.sh
@@ -1,20 +1,15 @@
 echo "############################################"
-echo "###         Protectctl install           ###"
+echo "###     tridentctl-protect install       ###"
 echo "############################################"
 
-ACCOUNTID=d13e74bf-3ced-488d-b31a-8ed307395e93
-TOKEN=hwTgNdnLXQEa8mD5h_YxOSuhBNakxQfA-npBgHU6Dsg=
-PROTECTVERSION=24.10.0-preview.1
-
-podman login -u $ACCOUNTID -p $TOKEN cr.astra.netapp.io
-podman pull cr.astra.netapp.io/trident-protectctl:$PROTECTVERSION
-podman run --rm -v $(pwd):/pctl-tmp cr.astra.netapp.io/trident-protectctl:$PROTECTVERSION cp protectctl /pctl-tmp
-mv protectctl /usr/local/bin
+curl -L -o tridentctl-protect https://github.com/NetApp/tridentctl-protect/releases/download/24.10.0/tridentctl-protect-linux-amd64
+chmod +x tridentctl-protect
+sudo mv ./tridentctl-protect /usr/local/bin/
 
 mkdir -p ~/.trident-protect
-echo 'namespace: trident-protect' > ~/.trident-protect/protectctl.yaml
 
-cat <<EOT >> ~/.bashrc
-source <(protectctl completion bash)
-EOT
-source ~/.bashrc
+
+curl -L -O https://github.com/NetApp/tridentctl-protect/releases/download/24.10.0/tridentctl-completion.bash
+mkdir -p ~/.bash/completions
+mv tridentctl-completion.bash ~/.bash/completions/
+source ~/.bash/completions/tridentctl-completion.bash
