5bedb9b6cbf0855a9bb74e94a2727c78beb3b883
Curtis Burchett <curtis@burchett.me>
2024-11-22 20:54:41 +0000

    Completed Exercise 5 Task 7

---
+++ Exercise 5/exercise5Task7-1.sh
@@ -0,0 +1,17 @@
+echo "############################################"
+echo "###     tridentctl-protect install       ###"
+echo "############################################"
+
+VERSION=24.10.0
+
+curl -L -o tridentctl-protect https://github.com/NetApp/tridentctl-protect/releases/download/$VERSION/tridentctl-protect-linux-amd64
+chmod +x tridentctl-protect
+sudo mv ./tridentctl-protect /usr/local/bin/
+
+mkdir -p ~/.trident-protect
+
+
+#curl -L -O https://github.com/NetApp/tridentctl-protect/releases/download/$VERSION/tridentctl-completion.bash
+mkdir -p ~/.bash/completions
+#mv tridentctl-completion.bash ~/.bash/completions/
+#source ~/.bash/completions/tridentctl-protect-completion.bash
