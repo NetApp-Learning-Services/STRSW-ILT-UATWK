6604f721c49e6151697f8add001895ec68128651
Curtis Burchett <curtis@burchett.me>
2024-11-23 20:48:47 +0000

    Removed old unneeded files

--- Extras/trident-protect demo/dest/exercise5Task2-1.sh
+++
@@ -1,6 +0,0 @@
-BUCKETKEY=DD3EQ31PWKMKZ9HAB3A8
-BUCKETSECRET=_3cnR9rXTztJB3gKBxuSeWAysZxPXTpq_DYdJsi9
-
-kubectl create secret generic -n trident-protect s3-dest-creds \
-  --from-literal=accessKeyID=$BUCKETKEY2 \
-  --from-literal=secretAccessKey=$BUCKETSECRET2
--- Extras/trident-protect demo/src/exercise5Task2-1.sh
+++
@@ -1,6 +0,0 @@
-BUCKETKEY=EWEF3217OICBTNOVDIGC
-BUCKETSECRET=_c1n_03Cp_zuV_tK3gjTqBz49ctaEWluFZ3GBdpA
-
-kubectl create secret generic -n trident-protect s3-src-creds \
-  --from-literal=accessKeyID=$BUCKETKEY \
-  --from-literal=secretAccessKey=$BUCKETSECRET
