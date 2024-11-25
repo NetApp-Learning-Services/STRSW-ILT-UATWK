f6e1b39b8d71551f3c045a9d41dd3620091f9e69
Curtis Burchett <curtis@burchett.me>
2024-11-25 19:54:06 +0000

    Made sure that ./exercise2Task4 works on Kubernetes cluster 2

--- Exercise 3/Prerequisites/exercise2Task4.sh
+++ Exercise 3/Prerequisites/exercise2Task4.sh
@@ -5,9 +5,23 @@
 #!/bin/bash
 sudo apt install sshpass
 for run in {1..3}; do
-    echo "Run: $run"
+    echo "Run: kubwor1-$run"
     echo "Step 1"
     sshpass -p Netapp1! scp -o "StrictHostKeyChecking=no" multipath.conf root@kubwor1-$run:/etc/multipath.conf
+    echo "Step 2"
+    sshpass -p Netapp1! ssh root@kubwor1-$run "sudo systemctl enable --now iscsid multipathd"
+    echo "Step 3"
+    sshpass -p Netapp1! ssh root@kubwor1-$run "cat /etc/iscsi/initiatorname.iscsi"
+    echo "Step 4"
+    sshpass -p Netapp1! ssh root@kubwor1-$run "sudo modprobe nvme-tcp"
+    echo "Step 5"
+    sshpass -p Netapp1! ssh root@kubwor1-$run "cat /etc/nvme/hostnqn"
+done
+
+for run in {1..2}; do
+    echo "Run: kubwor2-$run"
+    echo "Step 1"
+    sshpass -p Netapp1! scp -o "StrictHostKeyChecking=no" multipath.conf root@kubwor2-$run:/etc/multipath.conf
     echo "Step 2"
     sshpass -p Netapp1! ssh root@kubwor1-$run "sudo systemctl enable --now iscsid multipathd"
     echo "Step 3"
