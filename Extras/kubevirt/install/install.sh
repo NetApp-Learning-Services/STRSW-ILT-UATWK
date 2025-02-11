5e5c3231099c06e8fb4b496ac0472dc7d5fbd494
Curtis Burchett <curtis@burchett.me>
2025-02-11 23:52:30 +0000

    Kubevirt - working on cdi

--- Extras/kubevirt/install/install.sh
+++ Extras/kubevirt/install/install.sh
@@ -4,6 +4,12 @@
 
 #!/bin/bash
 
+DEST_DIR="/usr/local/bin"
+
+echo "############################################"
+echo "###          KubeVirt install            ###"
+echo "############################################"
+
 KUBEVIRT_VERSION=$(curl -s https://api.github.com/repos/kubevirt/kubevirt/releases/latest | awk -F '[ \t":]+' '/tag_name/ {print $3}')
 echo "KubeVirt version: $KUBEVIRT_VERSION"
 
@@ -11,7 +17,22 @@ kubectl create -f https://github.com/kubevirt/kubevirt/releases/download/${KUBEV
 
 kubectl create -f https://github.com/kubevirt/kubevirt/releases/download/${KUBEVIRT_VERSION}/kubevirt-cr.yaml 
 
+echo "############################################"
+echo "###          virtctl install             ###"
+echo "############################################"
+
 curl -Lo virtctl https://github.com/kubevirt/kubevirt/releases/download/${KUBEVIRT_VERSION}/virtctl-${KUBEVIRT_VERSION}-linux-amd64
 
 sudo chmod +x virtctl
-sudo mv virtctl /usr/local/bin
+sudo mv virtctl $DEST_DIR
+
+echo "File virtctl-${KUBEVIRT_VERSION}-linux-amd64 has been extracted, moved to $DEST_DIR, and made executable."
+
+echo "############################################"
+echo "###  Containerized Data Importer install ###"
+echo "############################################"
+
+export CDI_VERSION=$(curl -Ls https://github.com/kubevirt/containerized-data-importer/releases/latest | grep -m 1 -o "v[0-9]\.[0-9]*\.[0-9]*")
+echo "Containerized Data Importer version: $CDI_VERSION"
+
+kubectl create -f https://github.com/kubevirt/containerized-data-importer/releases/download/$CDI_VERSION/cdi-operator.yaml
