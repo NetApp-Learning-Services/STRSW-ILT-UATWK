789d03edecb2467c63d1ee9bd8f2cf99e18c5596
Curtis Burchett <curtis@burchett.me>
2026-03-19 22:02:28 +0000

    Completed Exercise 6

--- Exercise 6/exercise6Task1-9.sh
+++ Exercise 6/exercise6Task1-9.sh
@@ -1,21 +1,15 @@
-# This script has been written for this exercise environment 
-# and is not intented to be used in a production enviroment
-# Execute by: ./exercise6Task1-9.sh
+echo "############################################"
+echo "###      Trident Protect install         ###"
+echo "############################################"
 
-#!/bin/bash
-sudo apt install sshpass
+CLUSTERNAME=destination
+PROTECTNS=trident-protect
+PROTECTVERSION=100.2602.0
 
-for run in {1..2}; do
-    echo "Run: kubwor2-$run"
-    echo "Step 1"
-    sshpass -p Netapp1! scp -o "StrictHostKeyChecking=no" multipath.conf root@kubwor2-$run:/etc/multipath.conf
-    echo "Step 2"
-    sshpass -p Netapp1! ssh root@kubwor1-$run "sudo systemctl enable --now iscsid multipathd"
-    sshpass -p Netapp1! ssh root@kubwor1-$run "sudo service multipathd restart"
-    echo "Step 3"
-    sshpass -p Netapp1! ssh root@kubwor1-$run "cat /etc/iscsi/initiatorname.iscsi"
-    echo "Step 4"
-    sshpass -p Netapp1! ssh root@kubwor1-$run "sudo modprobe nvme-tcp"
-    echo "Step 5"
-    sshpass -p Netapp1! ssh root@kubwor1-$run "cat /etc/nvme/hostnqn"
-done
+helm repo add netapp-trident-protect https://netapp.github.io/trident-protect-helm-chart
+
+helm install trident-protect netapp-trident-protect/trident-protect \
+    --set clusterName=$CLUSTERNAME \
+    --version $PROTECTVERSION      \
+    --namespace $PROTECTNS         \
+    --create-namespace  
