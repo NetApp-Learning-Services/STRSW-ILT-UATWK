d4a076fbd1a92353ed3b825076cd28fac36b36b9
Curtis Burchett <curtis@burchett.me>
2026-03-25 17:55:12 +0000

    Completed Exercise 6

--- Exercise 6/Solutions/exercise6Task4-7.sh
+++ Exercise 6/Solutions/exercise6Task4-7.sh
@@ -3,11 +3,14 @@
 # Switch kubectl context to source-admin@source
 kubectl config use-context source-admin@source
 
-# Get the UUID of the application 'mywordpressapp' in the 'trident-protect' namespace
-uuid = kubectl -n trident-protect get applications mywordpressapp -o "jsonpath={.metadata.uid}"
+# Get the UUID of the application 'myredisapp' in the 'trident-protect' namespace
+uuid=$(kubectl -n myredisapp get applications myredisapp -o jsonpath='{.metadata.uid}')
 
 # Define the file to be modified
 file_path="exercise6Task4-7.yaml"
 
 # Use sed to replace 'change_me' with the value of the variable
 sed -i "s/change_me/$uuid/g" "$file_path"
+
+# Switch kubectl context to destination-admin@destination
+kubectl config use-context destination-admin@destination
